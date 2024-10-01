52c37433d20f14a86a00162d1e08212701ec5786
geliaswilson <geliaswilson@gmail.com>
2024-10-01 04:03:58 -0400

    create app.scss and import into app.jsx, apply global and variable partials in app.scss, provide li in ActualStorySquare.jsx className, build styling in ActualStorySquare.scss, build global partials styling, make primary red and secondary red in variables.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import 'react-tooltip/dist/react-tooltip.css';
+import "./App.scss";
 import Homepage from "./pages/Homepage/Homepage";
 import ActualStories from './pages/ActualStories/ActualStories';
 import Events from './pages/Events/Events';
