ea7db4f946d5c86422fa9b506419ca325e8dc7bc
geliaswilson <geliaswilson@gmail.com>
2024-10-02 04:01:53 -0400

    style chapter.jsx chapter part title and chapter part anchor

--- src/components/StoryContent/StoryContent.jsx
+++ src/components/StoryContent/StoryContent.jsx
@@ -11,7 +11,7 @@ const StoryContent = ({story, concepts}) => {
         <h1 className="actual-stories__title">{story.name}</h1>
         <p className="actual-stories__hook">{story.hook}</p>
         <ul className="navbar">
-            <li className="actual-stories__anchor-list">
+            <li className="actual-stories__anchor-list-item">
                 <AnchorLink className="actual-stories__anchor" href="#7c08defc-da13-4b76-81f3-4369c955ec61">
                 <p className="actual-stories__anchor-text">Replay</p>
                 </AnchorLink>
